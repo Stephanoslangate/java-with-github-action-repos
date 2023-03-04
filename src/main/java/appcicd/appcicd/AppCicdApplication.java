ecdd20d73d0af6e32a6f8ff7125668811554adbf
Stephanoslangate <nzoux01@gmail.com>
2023-03-04 04:10:53 +0100

    cv spring cicd 8

--- src/main/java/appcicd/appcicd/AppCicdApplication.java
+++ src/main/java/appcicd/appcicd/AppCicdApplication.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AppCicdApplication {
-	@GetMapping("/welcome")
-	public String welcome(){
-		return "Welcome to steph cacd";
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppCicdApplication.class, args);
 	}
